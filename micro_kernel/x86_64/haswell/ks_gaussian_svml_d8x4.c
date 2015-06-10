44155a1ab2cadb32e57a3cf1d58f5f2e6f436b49
Chenhan D. Yu <chenhan@cs.utexas.edu>
2015-06-10 17:56:14 -0400

    New Haswell gaussian_svml micro-kernel.
    Tested my macbook Intel Core i5 2.8 GHz/Turbo boost??, and we
    get about 30 GFLOPS.

---
+++ micro_kernel/x86_64/haswell/ks_gaussian_svml_d8x4.c
@@ -0,0 +1,346 @@
+#include <immintrin.h> // AVX
+#include <ks.h>
+
+
+void ks_gaussian_svml_d8x4(
+    int    k,
+    double alpha,
+    double *u,
+    double *aa,
+    double *a,
+    double *bb,
+    double *b,
+    double *w,
+    aux_t  *aux
+    )
+{
+  int    i;
+  double dzero = 0.0;
+  double neg2 = -2.0;
+  v4df_t c03_0, c03_1, c03_2, c03_3;
+  v4df_t c47_0, c47_1, c47_2, c47_3;
+  v4df_t tmpc03_0, tmpc03_1, tmpc03_2, tmpc03_3;
+  v4df_t tmpc47_0, tmpc47_1, tmpc47_2, tmpc47_3;
+  v4df_t c_tmp;
+  v4df_t u03;
+  v4df_t u47;
+  v4df_t a03, a47;
+  v4df_t A03, A47; // prefetched A 
+
+  v4df_t b0, b1, b2, b3;
+  v4df_t B0; // prefetched B
+
+
+
+  v4df_t tmp;
+  v4df_t aa_tmp, bb_tmp;
+  v4df_t w_tmp;
+
+
+  int k_iter = k / 2;
+  int k_left = k % 2;
+
+  __asm__ volatile( "prefetcht0 0(%0)    \n\t" : :"r"( a ) );
+  __asm__ volatile( "prefetcht2 0(%0)    \n\t" : :"r"( aux->b_next ) );
+
+
+  c03_0.v = _mm256_setzero_pd();
+  c03_1.v = _mm256_setzero_pd();
+  c03_2.v = _mm256_setzero_pd();
+  c03_3.v = _mm256_setzero_pd();
+  c47_0.v = _mm256_setzero_pd();
+  c47_1.v = _mm256_setzero_pd();
+  c47_2.v = _mm256_setzero_pd();
+  c47_3.v = _mm256_setzero_pd();
+
+
+  // Load a03
+  a03.v = _mm256_load_pd(      (double*)a         );
+  // Load a47
+  a47.v = _mm256_load_pd(      (double*)( a + 4 ) );
+  // Load (b0,b1,b2,b3)
+  b0.v  = _mm256_load_pd(      (double*)b         );
+
+  for ( i = 0; i < k_iter; ++i ) {
+    __asm__ volatile( "prefetcht0 192(%0)    \n\t" : :"r"(a) );
+
+    // Preload A03
+    A03.v = _mm256_load_pd(      (double*)( a + 8 ) );
+
+    c03_0.v = _mm256_fmadd_pd( a03.v, b0.v,  c03_0.v );
+    c47_0.v = _mm256_fmadd_pd( a47.v, b0.v,  c47_0.v );
+
+    // Preload A47
+    A47.v = _mm256_load_pd(      (double*)( a + 12 ) );
+
+    // Shuffle b ( 1, 0, 3, 2 )
+    b1.v  = _mm256_shuffle_pd( b0.v, b0.v, 0x5 );
+
+    c03_1.v = _mm256_fmadd_pd( a03.v, b1.v, c03_1.v );
+    c47_1.v = _mm256_fmadd_pd( a47.v, b1.v, c47_1.v );
+
+    // Permute b ( 3, 2, 1, 0 )
+    b2.v  = _mm256_permute2f128_pd( b1.v, b1.v, 0x1 );
+
+    // Preload B0
+    B0.v  = _mm256_load_pd(      (double*)( b + 4 ) );
+
+    c03_2.v = _mm256_fmadd_pd( a03.v, b2.v, c03_2.v );
+    c47_2.v = _mm256_fmadd_pd( a47.v, b2.v, c47_2.v );
+
+    // Shuffle b ( 3, 2, 1, 0 )
+    b3.v  = _mm256_shuffle_pd( b2.v, b2.v, 0x5 );
+
+    c03_3.v = _mm256_fmadd_pd( a03.v, b3.v, c03_3.v );
+    c47_3.v = _mm256_fmadd_pd( a47.v, b3.v, c47_3.v );
+
+
+    // Iteration #1
+    __asm__ volatile( "prefetcht0 512(%0)    \n\t" : :"r"(a) );
+
+    // Preload a03 ( next iteration )
+    a03.v = _mm256_load_pd(      (double*)( a + 16 ) );
+
+    c03_0.v = _mm256_fmadd_pd( A03.v, B0.v, c03_0.v );
+
+    b1.v  = _mm256_shuffle_pd( B0.v, B0.v, 0x5 );
+
+    c47_0.v = _mm256_fmadd_pd( A47.v, B0.v, c47_0.v );
+    c03_1.v = _mm256_fmadd_pd( A03.v, b1.v, c03_1.v );
+
+    // Preload a47 ( next iteration )
+    a47.v = _mm256_load_pd(      (double*)( a + 20 ) );
+
+    // Permute b ( 3, 2, 1, 0 )
+    b2.v  = _mm256_permute2f128_pd( b1.v, b1.v, 0x1 );
+
+    c47_1.v = _mm256_fmadd_pd( A47.v, b1.v, c47_1.v );
+    c03_2.v = _mm256_fmadd_pd( A03.v, b2.v, c03_2.v );
+
+    // Shuffle b ( 3, 2, 1, 0 )
+    b3.v  = _mm256_shuffle_pd( b2.v, b2.v, 0x5 );
+
+    c47_2.v = _mm256_fmadd_pd( A47.v, b2.v, c47_2.v );
+
+    // Load b0 ( next iteration )
+    b0.v  = _mm256_load_pd(      (double*)( b + 8 ) );
+
+    c03_3.v = _mm256_fmadd_pd( A03.v, b3.v, c03_3.v );
+    c47_3.v = _mm256_fmadd_pd( A47.v, b3.v, c47_3.v );
+
+    a += 16;
+    b += 8;
+  }
+
+  for ( i = 0; i < k_left; ++i ) {
+    a03.v = _mm256_load_pd(      (double*)a         );
+    //printf( "a03 = %lf, %lf, %lf, %lf\n", a03.d[0], a03.d[1], a03.d[2], a03.d[3] );
+
+    a47.v = _mm256_load_pd(      (double*)( a + 4 ) );
+    //printf( "a47 = %lf, %lf, %lf, %lf\n", a47.d[0], a47.d[1], a47.d[2], a47.d[3] );
+
+    b0.v  = _mm256_load_pd(      (double*)b         );
+    //printf( "b0  = %lf, %lf, %lf, %lf\n", b0.d[0], b0.d[1], b0.d[2], b0.d[3] );
+
+    c03_0.v = _mm256_fmadd_pd( a03.v, b0.v, c03_0.v );
+    c47_0.v = _mm256_fmadd_pd( a03.v, b0.v, c47_0.v );
+
+    // Shuffle b ( 1, 0, 3, 2 )
+    b1.v  = _mm256_shuffle_pd( b0.v, b0.v, 0x5 );
+
+    c03_1.v = _mm256_fmadd_pd( a03.v, b1.v, c03_1.v );
+    c47_1.v = _mm256_fmadd_pd( a47.v, b1.v, c47_1.v );
+
+    // Permute b ( 3, 2, 1, 0 )
+    b2.v  = _mm256_permute2f128_pd( b1.v, b1.v, 0x1 );
+
+    c03_2.v = _mm256_fmadd_pd( a03.v, b2.v, c03_2.v );
+    c47_2.v = _mm256_fmadd_pd( a47.v, b2.v, c47_2.v );
+
+    // Shuffle b ( 3, 2, 1, 0 )
+    b3.v  = _mm256_shuffle_pd( b2.v, b2.v, 0x5 );
+
+    c03_3.v = _mm256_fmadd_pd( a03.v, b3.v, c03_3.v );
+    c47_3.v = _mm256_fmadd_pd( a47.v, b3.v, c47_3.v );
+
+    a += 8;
+    b += 4;
+  }
+ 
+
+  // Prefetch aa and bb
+  __asm__ volatile( "prefetcht0 0(%0)    \n\t" : :"r"( aa ) );
+  __asm__ volatile( "prefetcht0 0(%0)    \n\t" : :"r"( bb ) );
+
+
+  tmpc03_0.v = _mm256_blend_pd( c03_0.v, c03_1.v, 0x6 );
+  tmpc03_1.v = _mm256_blend_pd( c03_1.v, c03_0.v, 0x6 );
+  
+  tmpc03_2.v = _mm256_blend_pd( c03_2.v, c03_3.v, 0x6 );
+  tmpc03_3.v = _mm256_blend_pd( c03_3.v, c03_2.v, 0x6 );
+
+  tmpc47_0.v = _mm256_blend_pd( c47_0.v, c47_1.v, 0x6 );
+  tmpc47_1.v = _mm256_blend_pd( c47_1.v, c47_0.v, 0x6 );
+
+  tmpc47_2.v = _mm256_blend_pd( c47_2.v, c47_3.v, 0x6 );
+  tmpc47_3.v = _mm256_blend_pd( c47_3.v, c47_2.v, 0x6 );
+
+  c03_0.v    = _mm256_permute2f128_pd( tmpc03_0.v, tmpc03_2.v, 0x30 );
+  c03_3.v    = _mm256_permute2f128_pd( tmpc03_2.v, tmpc03_0.v, 0x30 );
+
+  c03_1.v    = _mm256_permute2f128_pd( tmpc03_1.v, tmpc03_3.v, 0x30 );
+  c03_2.v    = _mm256_permute2f128_pd( tmpc03_3.v, tmpc03_1.v, 0x30 );
+
+  c47_0.v    = _mm256_permute2f128_pd( tmpc47_0.v, tmpc47_2.v, 0x30 );
+  c47_3.v    = _mm256_permute2f128_pd( tmpc47_2.v, tmpc47_0.v, 0x30 );
+
+  c47_1.v    = _mm256_permute2f128_pd( tmpc47_1.v, tmpc47_3.v, 0x30 );
+  c47_2.v    = _mm256_permute2f128_pd( tmpc47_3.v, tmpc47_1.v, 0x30 );
+
+  //printf( "%lf, %lf, %lf, %lf\n", c03_0.d[0], c03_1.d[0], c03_2.d[0], c03_3.d[0] );
+  //printf( "%lf, %lf, %lf, %lf\n", c03_0.d[1], c03_1.d[1], c03_2.d[1], c03_3.d[1] );
+  //printf( "%lf, %lf, %lf, %lf\n", c03_0.d[2], c03_1.d[2], c03_2.d[2], c03_3.d[2] );
+  //printf( "%lf, %lf, %lf, %lf\n", c03_0.d[3], c03_1.d[3], c03_2.d[3], c03_3.d[3] );
+  //printf( "%lf, %lf, %lf, %lf\n", c47_0.d[0], c47_1.d[0], c47_2.d[0], c47_3.d[0] );
+  //printf( "%lf, %lf, %lf, %lf\n", c47_0.d[1], c47_1.d[1], c47_2.d[1], c47_3.d[1] );
+  //printf( "%lf, %lf, %lf, %lf\n", c47_0.d[2], c47_1.d[2], c47_2.d[2], c47_3.d[2] );
+  //printf( "%lf, %lf, %lf, %lf\n", c47_0.d[3], c47_1.d[3], c47_2.d[3], c47_3.d[3] );
+
+
+
+  aa_tmp.v = _mm256_broadcast_sd( &neg2 );
+  c03_0.v  = _mm256_mul_pd( aa_tmp.v, c03_0.v );
+  c03_1.v  = _mm256_mul_pd( aa_tmp.v, c03_1.v );
+  c03_2.v  = _mm256_mul_pd( aa_tmp.v, c03_2.v );
+  c03_3.v  = _mm256_mul_pd( aa_tmp.v, c03_3.v );
+  c47_0.v  = _mm256_mul_pd( aa_tmp.v, c47_0.v );
+  c47_1.v  = _mm256_mul_pd( aa_tmp.v, c47_1.v );
+  c47_2.v  = _mm256_mul_pd( aa_tmp.v, c47_2.v );
+  c47_3.v  = _mm256_mul_pd( aa_tmp.v, c47_3.v );
+
+
+  //printf( "\n" );
+  //printf( "%lf, %lf, %lf, %lf\n", c03_0.d[0], c03_1.d[0], c03_2.d[0], c03_3.d[0] );
+  //printf( "%lf, %lf, %lf, %lf\n", c03_0.d[1], c03_1.d[1], c03_2.d[1], c03_3.d[1] );
+  //printf( "%lf, %lf, %lf, %lf\n", c03_0.d[2], c03_1.d[2], c03_2.d[2], c03_3.d[2] );
+  //printf( "%lf, %lf, %lf, %lf\n", c03_0.d[3], c03_1.d[3], c03_2.d[3], c03_3.d[3] );
+  //printf( "%lf, %lf, %lf, %lf\n", c47_0.d[0], c47_1.d[0], c47_2.d[0], c47_3.d[0] );
+  //printf( "%lf, %lf, %lf, %lf\n", c47_0.d[1], c47_1.d[1], c47_2.d[1], c47_3.d[1] );
+  //printf( "%lf, %lf, %lf, %lf\n", c47_0.d[2], c47_1.d[2], c47_2.d[2], c47_3.d[2] );
+  //printf( "%lf, %lf, %lf, %lf\n", c47_0.d[3], c47_1.d[3], c47_2.d[3], c47_3.d[3] );
+
+
+  aa_tmp.v = _mm256_load_pd( (double*)aa );
+  c03_0.v  = _mm256_add_pd( aa_tmp.v, c03_0.v );
+  c03_1.v  = _mm256_add_pd( aa_tmp.v, c03_1.v );
+  c03_2.v  = _mm256_add_pd( aa_tmp.v, c03_2.v );
+  c03_3.v  = _mm256_add_pd( aa_tmp.v, c03_3.v );
+
+  //printf( "aa03 = %lf, %lf, %lf, %lf\n", aa_tmp.d[0], aa_tmp.d[1], aa_tmp.d[2], aa_tmp.d[3] );
+  //printf( "bb03 = %lf, %lf, %lf, %lf\n", bb[ 0 ], bb[ 1 ], bb[ 2 ], bb[ 3 ] );
+
+  aa_tmp.v = _mm256_load_pd( (double*)( aa + 4 ) );
+  c47_0.v  = _mm256_add_pd( aa_tmp.v, c47_0.v );
+  c47_1.v  = _mm256_add_pd( aa_tmp.v, c47_1.v );
+  c47_2.v  = _mm256_add_pd( aa_tmp.v, c47_2.v );
+  c47_3.v  = _mm256_add_pd( aa_tmp.v, c47_3.v );
+  
+  bb_tmp.v = _mm256_broadcast_sd( (double*)bb );
+  c03_0.v  = _mm256_add_pd( bb_tmp.v, c03_0.v );
+  c47_0.v  = _mm256_add_pd( bb_tmp.v, c47_0.v );
+
+  bb_tmp.v = _mm256_broadcast_sd( (double*)( bb + 1 ) );
+  c03_1.v  = _mm256_add_pd( bb_tmp.v, c03_1.v );
+  c47_1.v  = _mm256_add_pd( bb_tmp.v, c47_1.v );
+
+  bb_tmp.v = _mm256_broadcast_sd( (double*)( bb + 2 ) );
+  c03_2.v  = _mm256_add_pd( bb_tmp.v, c03_2.v );
+  c47_2.v  = _mm256_add_pd( bb_tmp.v, c47_2.v );
+
+  bb_tmp.v = _mm256_broadcast_sd( (double*)( bb + 3 ) );
+  c03_3.v  = _mm256_add_pd( bb_tmp.v, c03_3.v );
+  c47_3.v  = _mm256_add_pd( bb_tmp.v, c47_3.v );
+
+
+  // Check if there is any illegle value 
+  tmp.v    = _mm256_broadcast_sd( &dzero );
+  c03_0.v  = _mm256_max_pd( tmp.v, c03_0.v );
+  c03_1.v  = _mm256_max_pd( tmp.v, c03_1.v );
+  c03_2.v  = _mm256_max_pd( tmp.v, c03_2.v );
+  c03_3.v  = _mm256_max_pd( tmp.v, c03_3.v );
+  c47_0.v  = _mm256_max_pd( tmp.v, c47_0.v );
+  c47_1.v  = _mm256_max_pd( tmp.v, c47_1.v );
+  c47_2.v  = _mm256_max_pd( tmp.v, c47_2.v );
+  c47_3.v  = _mm256_max_pd( tmp.v, c47_3.v );
+
+
+
+
+  aa_tmp.v = _mm256_broadcast_sd( &alpha );
+  c03_0.v  = _mm256_mul_pd( aa_tmp.v, c03_0.v );
+  c03_1.v  = _mm256_mul_pd( aa_tmp.v, c03_1.v );
+  c03_2.v  = _mm256_mul_pd( aa_tmp.v, c03_2.v );
+  c03_3.v  = _mm256_mul_pd( aa_tmp.v, c03_3.v );
+  c47_0.v  = _mm256_mul_pd( aa_tmp.v, c47_0.v );
+  c47_1.v  = _mm256_mul_pd( aa_tmp.v, c47_1.v );
+  c47_2.v  = _mm256_mul_pd( aa_tmp.v, c47_2.v );
+  c47_3.v  = _mm256_mul_pd( aa_tmp.v, c47_3.v );
+
+
+
+  // Prefetch u
+  __asm__ volatile( "prefetcht0 0(%0)    \n\t" : :"r"( u ) );
+
+  c03_0.v       = _mm256_exp_pd( c03_0.v );
+  c03_1.v       = _mm256_exp_pd( c03_1.v );
+
+  // Prefetch w
+  __asm__ volatile( "prefetcht0 0(%0)    \n\t" : :"r"( w ) );
+
+  c03_2.v       = _mm256_exp_pd( c03_2.v );
+  c03_3.v       = _mm256_exp_pd( c03_3.v );
+
+  // Preload u03
+  u03.v    = _mm256_load_pd( (double*)u );
+
+  c47_0.v       = _mm256_exp_pd( c47_0.v );
+  c47_1.v       = _mm256_exp_pd( c47_1.v );
+
+  // Preload u47
+  u47.v    = _mm256_load_pd( (double*)( u + 4 ) );
+
+  c47_2.v       = _mm256_exp_pd( c47_2.v );
+  c47_3.v       = _mm256_exp_pd( c47_3.v );
+
+
+  //u03.v    = _mm256_load_pd( (double*)u );
+  //u47.v    = _mm256_load_pd( (double*)( u + 4 ) );
+
+  w_tmp.v  = _mm256_broadcast_sd( (double*)w );
+  u03.v    = _mm256_fmadd_pd( w_tmp.v, c03_0.v, u03.v );
+  u47.v    = _mm256_fmadd_pd( w_tmp.v, c47_0.v, u47.v );
+  
+  w_tmp.v  = _mm256_broadcast_sd( (double*)( w + 1 ) );
+  u03.v    = _mm256_fmadd_pd( w_tmp.v, c03_1.v, u03.v );
+  u47.v    = _mm256_fmadd_pd( w_tmp.v, c47_1.v, u47.v );
+
+  w_tmp.v  = _mm256_broadcast_sd( (double*)( w + 2 ) );
+  u03.v    = _mm256_fmadd_pd( w_tmp.v, c03_2.v, u03.v );
+  u47.v    = _mm256_fmadd_pd( w_tmp.v, c47_2.v, u47.v );
+
+  w_tmp.v  = _mm256_broadcast_sd( (double*)( w + 3 ) );
+  u03.v    = _mm256_fmadd_pd( w_tmp.v, c03_3.v, u03.v );
+  u47.v    = _mm256_fmadd_pd( w_tmp.v, c47_3.v, u47.v );
+
+  _mm256_store_pd( (double*)u, u03.v );
+  _mm256_store_pd( (double*)( u + 4 ), u47.v );
+
+  //printf( "%lf\n", u03.d[0] );
+  //printf( "%lf\n", u03.d[1] );
+  //printf( "%lf\n", u03.d[2] );
+  //printf( "%lf\n", u03.d[3] );
+  //printf( "%lf\n", u47.d[0] );
+  //printf( "%lf\n", u47.d[1] );
+  //printf( "%lf\n", u47.d[2] );
+  //printf( "%lf\n", u47.d[3] );
+}
