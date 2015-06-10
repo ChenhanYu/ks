953334a6692fe7158500b29ecd91d723df6b93f3
Chenhan D. Yu <chenhan@cs.utexas.edu>
2015-06-10 17:22:15 -0400

    Minor modification

--- set_env_osx.sh
+++ set_env_osx.sh
@@ -3,7 +3,7 @@ export KS_DIR=$PWD
 echo "KS_DIR = $KS_DIR"
 
 export KMP_AFFINITY=compact
-export OMP_NUM_THREADS=1
+#export OMP_NUM_THREADS=1
 export KS_IC_NT=1
 export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/opt/intel/lib:/opt/intel/mkl/lib
 
