b6870dac6e62b8bee9eee5841e913611b4f20c05
Chenhan D. Yu <chenhan@cs.utexas.edu>
2015-06-21 08:17:04 -0500

    Enhance cmake functionality.
    Support both standard makefile and cmake.
    
    make.inc and make_osx.inc can later be deprecated.
    
    Now user only need to assign the MKL path and the target architecture
    in the set_env.sh file.

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,16 @@
 export KS_DIR=$PWD
 echo "KS_DIR = $KS_DIR"
 
-export KMP_AFFINITY=compact
-#export OMP_NUM_THREADS=1
-export KS_IC_NT=1
+# Manually set the target architecture.
+export KS_ARCH_MAJOR=x86_64
+export KS_ARCH_MINOR=sandybridge
+echo "KS_ARCH = $KS_ARCH_MAJOR/$KS_ARCH_MINOR"
 
+# Manually set the mkl path
+export KS_MKL_DIR=/opt/intel/mkl
+echo "KS_MKL_DIR = $KS_MKL_DIR"
+
+# Parallel options
+export KMP_AFFINITY=compact
+export OMP_NUM_THREADS=1
+export KS_IC_NT=1
--- set_env_tacc.sh
+++ set_env_tacc.sh
@@ -2,15 +2,19 @@
 export KS_DIR=$PWD
 echo "KS_DIR = $KS_DIR"
 
-
 # Manually set the target architecture.
 export KS_ARCH_MAJOR=x86_64
 export KS_ARCH_MINOR=sandybridge
 echo "KS_ARCH = $KS_ARCH_MAJOR/$KS_ARCH_MINOR"
 
 
+# Load the Intel and the cmake module.
+module load intel
+module load cmake
+
+
 # Manually set the mkl path
-export KS_MKL_DIR=/opt/intel/mkl
+export KS_MKL_DIR=$TACC_MKL_DIR
 echo "KS_MKL_DIR = $KS_MKL_DIR"
 
 
@@ -18,7 +22,3 @@ echo "KS_MKL_DIR = $KS_MKL_DIR"
 export KMP_AFFINITY=compact
 export OMP_NUM_THREADS=1
 export KS_IC_NT=1
-
-
-# Add search paths.
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/opt/intel/lib:${KS_MKL_DIR}/lib
